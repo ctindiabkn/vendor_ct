782447eeb22fb3175e97562a7bfc3552cf8548b9
Vinesh Iyappan <ivinesh1997@gmail.com>
2019-12-26 05:32:58 +0000

    add prebuilts for core-techies
    Signed-off-by: theHari08 <ivinesh1997@gmail.com>

---
+++ config.mk
@@ -0,0 +1,36 @@
+PRODUCT_COPY_FILES += \
+    vendor/sha/etc/default-permissions/protext-permissions.xml:system/etc/default-permissions/protext-permissions.xml \
+    vendor/sha/etc/permissions/privapp-permissions-protext.xml:system/etc/permissions/privapp-permissions-protext.xml
+
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    Alarm \
+    Aliexpress \
+    Amazon \
+    Anysoft \
+    Blockinger \
+    Calculator \
+    Facebook \
+    FrozenBubble \
+    Gmail \
+    Googletranslate \
+    Instagram \
+    Messenger \
+	Netflix \
+	Outlook \
+	Shazam \
+	Skype \
+	Snapchat \
+	Solitaire \
+	Spotify \
+	SSChat \
+	SSMail \
+	Twitter \
+	Uber \
+	Whatsapp \
+	Youtube
+
+# Prebuilt Packages
+PRODUCT_PACKAGES += \
+    Calendar \
+    SetupWizard
